b6429dc0a07558b78a4237fbe3253c55f7a1de93
Mikhail Molotkov <mmolotkov1@sheffield.ac.uk>
2017-01-27 16:15:31 +0000

    added test cases

--- text_analysis_module/tests.py
+++ text_analysis_module/tests.py
@@ -1,9 +1,9 @@
 # Import packages
 import unittest2 as unittest
-from text_analysis import sentiment_analysis
+from text_analysis import sentiment_analysis, lexical_density_and_readability_analysis
 # Tests go here
 class TestTextAnalysis(unittest.TestCase):
-    
+
     def test_sentiment_analysis_negative(self):
         score = sentiment_analysis("It was one of the worst movies I've seen, despite good reviews. \
         Unbelievably bad acting!! Poor direction. VERY poor production. \
@@ -15,6 +15,15 @@ class TestTextAnalysis(unittest.TestCase):
         VADER is VERY SMART, handsome, and FUNNY. The movie was too good. \
         Sentiment analysis with VADER has never been this good. VADER is smart, handsome, and funny!")
         self.assertGreaterEqual(score, 0)
+    def test_lexical_density_and_readability_analysis(self):
+        density, readability = lexical_density_and_readability_analysis("VADER is VERY SMART, really handsome, and INCREDIBLY FUNNY!!! \
+        The book was good. \
+        VADER is VERY SMART, handsome, and FUNNY. The movie was too good. \
+        Sentiment analysis with VADER has never been this good. VADER is smart, handsome, and funny!")
+        self.assertNotEqual(density,0)
+        self.assertIsNotNone(density)
+        self.assertNotEqual(readability,0)
+        self.assertIsNotNone(readability)
 
 # To allow output in console
 if __name__ == '__main__':
