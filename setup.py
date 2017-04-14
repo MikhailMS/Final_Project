01b90c4b04766fec9ba9a3a47b2b2f8d9d820b42
Mikhail Molotkov <mmolotkov1@sheffield.ac.uk>
2017-04-14 01:00:15 +0100

    Release-version

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from setuptools import setup
 
 setup(
     name="TextMusification",
-    version="0.0.0",
+    version="1.0.0",
     description='Tool generates music depending on the text provided to it',
     author='Mikhail Molotkov',
     author_email='mmolotkov1@sheffield.ac.uk',
     url='https://github.com/MikhailMS/Final_Project',
     install_requires=[
         'Theano',
-        'theano-lstm'
+        'theano-lstm',
         'numpy >= 1.7.1',
         'scipy >= 0.11',
-        'scikit-learn'
+        'scikit-learn',
+        'sklearn',
         'sphinx >= 0.5.1',
         'pretty_midi',
         'python-midi',
