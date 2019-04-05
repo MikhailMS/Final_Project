69d7ec2101cde87d6aa7bf5929db47edb0c72ade
MikhailMS <1488maiklm@gmail.com>
2019-04-06 00:22:48 +0100

    Comment out broken code; Add CLI parser

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import argparse
+
 # Import modules
-from text_analysis_module import run_text_analysis_in_parallel, plot_features
+from text_analysis_module import run_text_analysis_in_parallel # , plot_features
 from music_generation_module import run_complexity_analysis, run_music_composition, run_tests, music_analysis
 from mapping_module import map_text_parameters
 
+
 # Main class
-def run_application():
+def run_application(test_enabled='false'):
     """Main function that runs whole application:
     Starting with text analysis -> train LSTM model ->
     Use text features to generate music
     It is advised to keep group_size and music_length parameters of same value
     """
     # Run text analysis (using multiprocessing package)
-    run_text_analysis_in_parallel()
+    # TODO: This method contains some bugs, so disable for now
+    # run_text_analysis_in_parallel()
 
     # Run text feature mapping onto music parameters
     sent_score, lex_score, read_score = map_text_parameters(group_size=5)
@@ -25,8 +29,14 @@ def run_application():
     # Run music complexity analysis
     complexity_stats = run_complexity_analysis(visual=False)
 
-    # Run final tests to ensure music has been generated correctly
-    run_tests(complexity_stats, sent_score_mapped=sent_score, read_score_mapped=read_score) # - FOR TEST PURPOSES
+    if test_enabled == 'true':
+        # Run final tests to ensure music has been generated correctly
+        run_tests(complexity_stats, sent_score_mapped=sent_score, read_score_mapped=read_score) # - FOR TEST PURPOSES
 
 if __name__ == '__main__':
-    run_application() # - ONLY COMMAND TO EXIST HERE
+    parser = argparse.ArgumentParser(description='Music composition CLI')
+    parser.add_argument('-t', '--test', metavar='', type=str, dest='test_enabled', help='Set to true, if you want to test composed music', default='false')
+
+    args = parser.parse_args()
+
+    run_application(test_enabled=args.test_enabled) # - ONLY COMMAND TO EXIST HERE
